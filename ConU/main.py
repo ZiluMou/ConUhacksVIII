ba17477e18bfafc42091565b7893f3d259227c28
sakil0518 <62127364+sakil0518@users.noreply.github.com>
2024-01-20 18:02:58 -0500

    Add files via upload

---
+++ ConU/main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Welcome to our GPT!')
+
+st.text('Did you struggle to check who is teaching the class?')
+st.text('Did you search every professor in Rate My Professor?')
+
