bc741a7cde473c3e432aba7bf8121ece91409b7e
sakil0518 <62127364+sakil0518@users.noreply.github.com>
2024-01-20 23:09:44 -0500

    Update main.py
    first version of tutorial website

--- ConU/main.py
+++ ConU/main.py
@@ -1,7 +1,83 @@
 import streamlit as st
 
-st.title('Welcome to our GPT!')
+st.set_page_config(
+    page_title="Intro to ConUGPT",
+    page_icon="👋",
+)
 
-st.text('Did you struggle to check who is teaching the class?')
-st.text('Did you search every professor in Rate My Professor?')
 
+with st.sidebar:
+    st.text("hello")
+
+st.title('Welcome to our ConUGPT!')
+st.markdown('''
+A customized GPT that helps students find information about their Concorida teachers. 
+With this app, you can easily find out which professors are teaching a particular course, 
+and even learn about the ratings of individual teachers. 
+This app is perfect for students who want to stay informed about their teachers and make informed decisions about their education. 
+With its intuitive interface and powerful search capabilities, you will shine this semester! ''')
+st.divider()
+
+
+st.markdown('### :thinking_face: Have you ever thought...')
+st.markdown("")
+
+col1, col2, col3 = st.columns(3, gap='medium')
+
+with col1:
+    st.write("I wish I can see the corresponding teachers of each class...I have to go all the way to the enrollment page to see the instructures :(")
+    st.image("img/search.jpg")
+    
+with col2:
+   st.write("I want to know who is the best teacher of the class. ")
+   st.image("img/survey.jpg")
+
+with col3:
+    st.write("It's very annoying to search every single teacher, making tons of tabs!")
+    st.image("img/stress.jpg")
+
+st.divider()
+
+
+st.markdown("### :thumbsup: We are here to help you!")
+
+st.markdown("#### :question: How to")
+st.write("Very simple. Just ask what you are looking for!")
+
+st.markdown("#### :question:  What kind of questions can I ask?")
+st.write("Below are the examples of the commands:")
+
+with st.chat_message("user"):
+    st.write("Who are the teachers of comp 233? Who has the best rate among them?")
+with st.chat_message("ai"):
+    st.write("John Doe, Charlie Walsh, and Logan Hart are teaching comp 233. John Doe has the best rate of 4.3.")
+st.markdown("")
+
+with st.chat_message("user"):
+    st.write("What are some classes taught by John Doe?")
+with st.chat_message("ai"):
+    st.write("John Doe teaches comp 233 and comp 248 this semester. ")
+st.markdown("")
+
+with st.chat_message("user"):
+    st.write("What is the name of the class with code 'comp 335'?")
+with st.chat_message("ai"):
+    st.write(" 'Introduction to Theoretical Computer Science' is the name of the class.")
+st.markdown("")
+
+st.write("You can also ask general info about the school.")
+with st.chat_message("user"):
+    st.write("When was Concordia founded?")
+with st.chat_message("ai"):
+    st.write("Concordia University was founded on August 24, 1974.")
+st.markdown("")
+
+st.markdown("#### :exclamation: Increase the accuracy")
+st.info('''change your prompt in case you suspect you did not get a complete response
+
+            Example: write "who are the teachers for soen 287?" 
+                     instead of "who teaches soen 287?"
+''')
+st.markdown("")
+
+st.link_button("Click here to try ConUGPT!", "https://streamlit.io/gallery", type='primary')
